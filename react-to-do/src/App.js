42f6b67161b3af835bad389d0e087cc6b76730aa
Adam King <kingadam7@gmail.com>
2018-12-16 13:52:25 -0800

    Checkpoint rtd-events: completed

--- react-to-do/src/App.js
+++ react-to-do/src/App.js
@@ -7,21 +7,40 @@ import ToDo from './components/ToDo.js';
      super(props);
      this.state = {
        todos: [
-         { description : 'Walk the cat', isCompleted: true },
+         { description : "Walk the cat", isCompleted: true },
          { description : "Throw the dishes away", isCompleted: false },
          { description : "Buy new dishes", isCompleted: false }
-       ]
+       ],
+       newTodoDescription: ''
 
      };
    }
+
+   handleSubmit(e) {
+     e.preventDefault();
+     console.log("handleSubmit called");
+   }
+
+   toggleComplete(index) {
+    const todos = this.state.todos.slice();
+    const todo = todos[index];
+    todo.isCompleted = todo.isCompleted ? false : true;
+    this.setState({ todos: todos });
+  }
+
+
    render() {
      return (
        <div className="App">
         <ul>
         { this.state.todos.map( (todo, index) =>
-         <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted} />
+         <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted} toggleComplete={ () => this.toggleComplete(index) } />
        )}
         </ul>
+        <form onSubmit={ (e) => this.handleSubmit(e) }>
+          <input type="text" value={ this.state.newTodoDescription } onChange= { (e) => this.handleChange(e) } />
+          <input type="submit" />
+        </form>
        </div>
      );
    }
